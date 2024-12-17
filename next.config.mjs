fb51b5fd2cdfcc9d0797a13ed5c29274b5ea1191
Yanfran <yanfranblancosalas@gmail.com>
2024-12-17 19:32:58 -0400

    avances 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    trailingSlash: true, // Opcional, para añadir una barra final a las rutas
+    images: {
+      unoptimized: true, // Evita optimización de imágenes no compatible con exportación
+    },
     // reactStrictMode: false,
 };
 
