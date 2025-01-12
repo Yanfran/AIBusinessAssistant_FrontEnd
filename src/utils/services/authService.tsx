ba78b46f3c6c59bd587773ab847150274b7e4be3
Yanfran <yanfranblancosalas@gmail.com>
2025-01-12 16:06:41 -0400

    Avances

--- src/utils/services/authService.tsx
+++ src/utils/services/authService.tsx
@@ -21,6 +21,9 @@ export const logout = async () => {
         method: "POST",
         headers: {
           Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
         },
       });
   
